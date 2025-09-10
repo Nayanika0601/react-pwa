085b1bed70594b945f05ebd5baf160159a71cd4d
Nayanika0601 <127814787+Nayanika0601@users.noreply.github.com>
2025-09-10 10:33:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: change this to '/<REPO>/' when deploying to GitHub Pages project URL
-  base: '/',
+  base: 'react-pwa',
 })
